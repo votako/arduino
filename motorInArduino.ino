c9b39594d0373681dce58bcdabea3426bb42f523
votako <votako@gmail.com>
2016-10-21 17:32:22 +0300

    added 3 buttons. 1-2 to move motor1, 3-4 to move motor2. 5 button to make step. made function oneStep

--- motorInArduino.ino
+++ motorInArduino.ino
@@ -13,10 +13,12 @@ int motorPin8 = 52; // Orange / Оранжевый - 28BYJ48 pin 4
 int buttonPin1 = 22; // кнопку подключаем к Pin 22.
 int buttonPin2 = 24; // кнопку подключаем к Pin 24.
 int buttonPin3 = 26; // кнопку подключаем к Pin 26.
+int buttonPin4 = 28; // кнопку подключаем к Pin 28.
+int buttonPin5 = 30; // кнопку подключаем к Pin 12.
 
 // установить скорость шагового двигателя.
 //variable to set stepper speed.
-int motorSpeed = 1200;
+int motorSpeed = 1800;
 
 //кол-во шагов
 int steps = 0;
@@ -43,31 +45,39 @@ void setup() {
   pinMode(buttonPin1, INPUT);
   pinMode(buttonPin2, INPUT);
   pinMode(buttonPin3, INPUT);
+  pinMode(buttonPin4, INPUT);
+  pinMode(buttonPin5, INPUT);
   digitalWrite(buttonPin1, HIGH);
   digitalWrite(buttonPin2, HIGH);
   digitalWrite(buttonPin3, HIGH);
+  digitalWrite(buttonPin4, HIGH);
+  digitalWrite(buttonPin5, HIGH);
 // Serial.begin(9600); //WHY comented?!
 }
 
 void loop(){
   
   //если кнопка1 и                   && steps < НУЖНОГО тогда поворачиваем
-  if (digitalRead(buttonPin1) == LOW && steps <=120){
+  if (digitalRead(buttonPin1) == LOW){// && steps <=70){
     anticlockwise(); // крутим влево если нажата кнопка 1. и увеличиваем кол-во шагов вращая(когда кнопка нажата это 1 сигнал или много? проверить)  
-    steps++;
+    //steps++;
   }
-  if (digitalRead(buttonPin2) == LOW && steps >=-120){
+  if (digitalRead(buttonPin2) == LOW){// && steps >=-70){
     //if(steps>0)steps=0;
     clockwise(); // крутим влево если нажата кнопка 2.
-    steps--;
+    //steps--;
+  }
+  if (digitalRead(buttonPin3) == LOW){// && steps <=70){
+    anticlockwise1(); // крутим влево если нажата кнопка 1. и увеличиваем кол-во шагов вращая(когда кнопка нажата это 1 сигнал или много? проверить)  
+    //steps++;
+  }
+  if (digitalRead(buttonPin4) == LOW){// && steps >=-70){
+    //if(steps>0)steps=0;
+    clockwise1(); // крутим влево если нажата кнопка 2.
+    //steps--;
   }//шаг вперед-назад
-  if (digitalRead(buttonPin3) == LOW){
-    for(int i =0; i<=120; i++){
-      anticlockwise();
-    }
-    for(int i =0; i<=120; i++){
-      clockwise();
-    }
+  if (digitalRead(buttonPin5) == LOW){
+    oneStep();
   }
 }
 
@@ -78,22 +88,51 @@ void loop(){
 // функция поворачивает мотор против часовой стрелки.
 void anticlockwise() {
   for(int i = 0; i < 8; i++) {
-    Serial.println('1buttonPresed'); 
     setOutput(i);
+    delayMicroseconds(motorSpeed);
+    //steps = steps++;
+  }
+}
+
+void anticlockwise1() {
+  for(int i = 0; i < 8; i++) {
     setOutput1(i);
     delayMicroseconds(motorSpeed);
-    steps = steps++;
+    //steps = steps++;
   }
 }
 // функция поворачивает мотор по часовой стрелке.
 void clockwise() {
   for(int i = 7; i >= 0; i--)  {
-      Serial.println('2buttonPresed');
       setOutput(i);
+      delayMicroseconds(motorSpeed);
+      //steps = steps++;  
+  }
+}
+
+void clockwise1() {
+  for(int i = 7; i >= 0; i--)  {
       setOutput1(i);
       delayMicroseconds(motorSpeed);
-      steps = steps++;  
+      //steps = steps++;  
   }
+}
+
+void oneStep(){
+    for(int i =0; i<=40; i++){
+      //подняли ногу
+      clockwise1();
+      //повернули ногу
+      anticlockwise();
+    }
+    for(int i =0; i<=40; i++){
+      //опустили ногу
+      anticlockwise1();      
+    }
+    for(int i =0; i<=40; i++){
+      //повернули
+      clockwise();    
+    }
 }
 // посылает куски массива на пины тем самым подавая ток? с 0 до... и с ... до 0? и подходящее битовое значение заставляет вращатся? о_О магия бладж
 void setOutput(int out) {
